d931a766e7122db9ea49253e93ae6a43f3ec8d7f
Salma Alam-Naylor <salma.alam-naylor@codecomputerlove.com>
2020-08-30 20:01:17 +0100

    Ingrated tmi.js

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,50 @@
-import * as express from 'express';
-import * as http from 'http';
-import * as WebSocket from 'ws';
+import express from 'express';
+import http from 'http';
+import WebSocket from 'ws';
+import tmi, { ChatUserstate } from 'tmi.js';
+
+const client = new (tmi.Client as any)({
+  options: { debug: true },
+  connection: {
+    secure: true,
+    reconnect: true,
+  },
+  channels: ['whitep4nth3r'],
+});
+
+client.connect();
 
 const app = express();
 
 //initialize a simple http server
 const server = http.createServer(app);
 
+//wss - web socket server
+//ws - web socket client
+
 //initialize the WebSocket server instance
 const wss = new WebSocket.Server({ server });
 
-wss.on('connection', (ws: WebSocket) => {
+interface ExtWebSocket extends WebSocket {
+  isAlive: boolean;
+  // ping: () => {};
+}
+
+wss.on('connection', (ws: ExtWebSocket) => {
+  ws.isAlive = true;
+
+  ws.on('pong', () => {
+    ws.isAlive = true;
+  });
+
+  client.on(
+    'message',
+    (channel: string, tags: ChatUserstate, message: string, self: boolean) => {
+      // "Alca: Hello, World!"
+      console.log(`${tags['display-name']}: ${message}`);
+    }
+  );
+
   //connection is up, let's add a simple simple event
   ws.on('message', (message: string) => {
     //log the received message and send it back to the client
@@ -32,6 +66,19 @@ wss.on('connection', (ws: WebSocket) => {
     } else {
       ws.send(`Hello, you sent -> ${message}`);
     }
+  });
+
+  const ping = setInterval(() => {
+    wss.clients.forEach((ws: any) => {
+      if (!ws.isAlive) return ws.terminate();
+
+      ws.isAlive = false;
+      ws.ping(() => {});
+    });
+  }, 10000);
+
+  ws.on('close', function (cc: number, cmsg: string) {
+    clearInterval(ping);
   });
 
   //send immediatly a feedback to the incoming connection
