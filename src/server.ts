9cc90582c6da9ad7c0fe31ac48e31e584ab96dbf
Salma Alam-Naylor <salma.alam-naylor@codecomputerlove.com>
2020-08-30 19:03:55 +0100

    Initial commit

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import * as express from 'express';
+import * as http from 'http';
+import * as WebSocket from 'ws';
+
+const app = express();
+
+//initialize a simple http server
+const server = http.createServer(app);
+
+//initialize the WebSocket server instance
+const wss = new WebSocket.Server({ server });
+
+wss.on('connection', (ws: WebSocket) => {
+  //connection is up, let's add a simple simple event
+  ws.on('message', (message: string) => {
+    //log the received message and send it back to the client
+    console.log('received: %s', message);
+
+    // arbitrary way to send categorised messages?
+    const broadcastRegex = /^broadcast\:/;
+
+    if (broadcastRegex.test(message)) {
+      message = message.replace(broadcastRegex, '');
+
+      //send back the message to the other clients
+      console.log(wss.clients);
+      wss.clients.forEach((client) => {
+        // if (client != ws) {
+        client.send(`Hello, broadcast message -> ${message}`);
+        // }
+      });
+    } else {
+      ws.send(`Hello, you sent -> ${message}`);
+    }
+  });
+
+  //send immediatly a feedback to the incoming connection
+  ws.send('Hi there, I am a WebSocket server');
+});
+
+//start our server
+server.listen(process.env.PORT || 8999, () => {
+  console.log(`Server started on port ${(server.address() as any).port} :)`);
+});
