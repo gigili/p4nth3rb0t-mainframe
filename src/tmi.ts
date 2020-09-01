cdc331d43cd9a1687dafca6bdba629ccaa35148d
Salma Alam-Naylor <salma.alam-naylor@codecomputerlove.com>
2020-09-01 19:43:18 +0100

    Added listeners for all sub events

--- src/tmi.ts
+++ src/tmi.ts
@@ -1,8 +1,10 @@
-import { Client, ChatUserstate } from "tmi.js";
-import { wsServer } from "./websocket";
+import { ChatUserstate } from 'tmi.js';
+import { ChatUserstate } from 'tmi.js';
+import { Client, ChatUserstate } from 'tmi.js';
+import { wsServer } from './websocket';
 
 export const tmi = new (Client as any)({
-  options: { debug: process.env.NODE_ENV === "development" ? true : false },
+  options: { debug: process.env.NODE_ENV === 'development' ? true : false },
   connection: {
     secure: true,
     reconnect: true,
@@ -19,7 +21,7 @@ export const tmi = new (Client as any)({
 // And then get all p4nth3rball and p4nth3rdrop to listen for those events
 
 tmi.on(
-  "message",
+  'message',
   (channel: string, tags: ChatUserstate, message: string, self: boolean) => {
     wsServer.clients.forEach((client) => {
       client.send(message);
@@ -27,8 +29,71 @@ tmi.on(
   }
 );
 
-tmi.on("join", (channel: string, username: string, self: boolean) => {
+tmi.on('join', (channel: string, username: string, self: boolean) => {
   wsServer.clients.forEach((client) => {
     client.send(`joined': ${username}`);
   });
 });
+
+/* Subscriptions */
+tmi.on(
+  'anongiftpaidupgrade',
+  async (channel: string, username: string, userstate: ChatUserstate) => {}
+);
+
+tmi.on(
+  'giftpaidupgrade',
+  async (
+    channel: string,
+    username: string,
+    sender: string,
+    userstate: ChatUserstate
+  ) => {}
+);
+
+tmi.on(
+  'subgift',
+  (
+    channel: string,
+    username: string,
+    streakMonths: number,
+    recipient: string,
+    methods: {},
+    userstate: ChatUserstate
+  ) => {}
+);
+
+tmi.on(
+  'subscription',
+  (
+    channel: string,
+    username: string,
+    recipient: string,
+    methods: {},
+    message: string,
+    userstate: ChatUserstate
+  ) => {}
+);
+
+tmi.on(
+  'resub',
+  (
+    channel: string,
+    username: string,
+    months: number,
+    message: string,
+    userstate: ChatUserstate,
+    methods: {}
+  ) => {}
+);
+
+tmi.on(
+  'submysterygift',
+  (
+    channel: string,
+    username: string,
+    numbOfSubs: number,
+    methods: {},
+    userstate: ChatUserstate
+  ) => {}
+);
