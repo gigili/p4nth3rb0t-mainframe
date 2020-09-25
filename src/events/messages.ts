23aca1be8b896c8db4868b2b7d958eba647374f1
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2020-09-25 14:17:55 +0100

    Extend the LC SO to include stream name and category

--- src/events/messages.ts
+++ src/events/messages.ts
@@ -5,12 +5,16 @@ import { config } from "../config";
 import { Packet, TwitchEvent } from "../types";
 import { getCommandFromMessage, ChatCommands } from "../utils/commands";
 import { isSillyQuestion } from "../utils/sillyQuestions";
-import UserManager from "../users/UserManager";
-import LiveCoders, { Coders, Coder } from "../users/LiveCoders";
+import { TwitchChannel, Coders, Coder } from "./../types";
 
+import { testConfig } from "../../testConfig";
+
+import UserManager from "../users/UserManager";
 const userManager = new UserManager();
 
+import LiveCoders from "../users/LiveCoders";
 const liveCoders = new LiveCoders();
+
 let teamMembers: Coders = [];
 const teamMembersGreeted: Coders = [];
 
@@ -61,11 +65,14 @@ tmi.on(
       !teamMembersGreeted.includes(possibleLiveCoder) &&
       possibleLiveCoder.name !== config.broadcaster
     ) {
-      tmi.say(
-        config.channel,
-        liveCoders.getWelcomeMessage(possibleLiveCoder.name)
+      const liveCoderChannel = await liveCoders.getChannelById(
+        possibleLiveCoder.id
       );
 
+      tmi.say(
+        config.channel,
+        liveCoders.getWelcomeMessage(liveCoderChannel as TwitchChannel)
+      );
       teamMembersGreeted.push(possibleLiveCoder);
       sendLiveCoderJoinEvent(possibleLiveCoder);
     }
--- src/users/LiveCoders.ts
+++ src/users/LiveCoders.ts
@@ -1,53 +1,98 @@
 import axios from "axios";
-
-export type Coder = {
-  name: string;
-  id: string;
-};
-
-export type Coders = Coder[];
-
-type TeamResponse = {
-  data: {
-    users: [
-      {
-        name: string;
-        _id: string;
-      }
-    ];
-  };
-};
+import {
+  TwitchChannel,
+  TeamResponse,
+  AccessTokenResponse,
+  Coders,
+} from "./../types";
 
 export default class LiveCoders {
   //TODO: some kind of cache expiry
+  //TODO: put channels in cache
   cache: Coders = [];
+  accessTokenCache = "";
 
-  public getWelcomeMessage = (username: string): string => {
-    return `whitep30PEWPEW Live Coder team member detected! 👋 Hello there, @${username}! Check out their channel here: https://twitch.tv/${username}`;
+  async getAccessToken(): Promise<string> {
+    if (this.accessTokenCache.length > 0) {
+      return this.accessTokenCache;
+    }
+
+    try {
+      const response = await axios.post<string, AccessTokenResponse>(
+        `https://id.twitch.tv/oauth2/token?scope=user:edit&response_type=token&client_secret=${process.env.CLIENT_SECRET}&grant_type=client_credentials&client_id=${process.env.CLIENT_ID}`,
+        {
+          headers: {
+            accept: "application/vnd.twitchtv.v5+json",
+          },
+        }
+      );
+
+      this.accessTokenCache = response.data.access_token;
+      return response.data.access_token;
+    } catch (error) {
+      console.log(error);
+    }
+
+    return "";
+  }
+
+  public getWelcomeMessage = (channel: TwitchChannel): string => {
+    return `whitep30PEWPEW Live Coder team member detected! 
+    👋 Hello there, @${channel.broadcaster_name}! 
+    Check out their channel here: https://twitch.tv/${channel.broadcaster_name} 
+    | They were last seen streaming ${channel.title} in ${channel.game_name} whitep30PEWPEW`;
   };
+
+  async getChannelById(
+    broadcasterId: string
+  ): Promise<TwitchChannel | undefined> {
+    const accessToken = await this.getAccessToken();
+
+    try {
+      const response = await axios.get<{ data: TwitchChannel[] }>(
+        `https://api.twitch.tv/helix/channels?broadcaster_id=${broadcasterId}`,
+        {
+          headers: {
+            Authorization: `Bearer ${accessToken}`,
+            "Client-Id": process.env.CLIENT_ID,
+          },
+        }
+      );
+
+      return response.data.data[0];
+    } catch (error) {
+      console.log(error);
+    }
+  }
 
   async getUserNames(): Promise<Coders> {
     if (this.cache.length > 0) {
       return this.cache;
     }
 
-    const response = await axios.get<any, TeamResponse>(
-      "https://api.twitch.tv/kraken/teams/livecoders",
-      {
-        headers: {
-          "client-id": process.env.CLIENT_ID,
-          accept: "application/vnd.twitchtv.v5+json",
-        },
-      }
-    );
+    try {
+      const response = await axios.get<any, TeamResponse>(
+        "https://api.twitch.tv/kraken/teams/livecoders",
+        {
+          headers: {
+            accept: "application/vnd.twitchtv.v5+json",
+            "client-id": process.env.CLIENT_ID,
+          },
+        }
+      );
 
-    const users = response.data.users.map((user) => ({
-      name: user.name,
-      id: user._id,
-    }));
+      const users = response.data.users.map((user) => ({
+        name: user.name,
+        id: user._id,
+      }));
 
-    this.cache = users;
+      this.cache = users;
 
-    return users;
+      return users;
+    } catch (error) {
+      console.log(error);
+    }
+
+    return [];
   }
 }
