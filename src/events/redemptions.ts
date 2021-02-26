e4eddc228104086004238b71e2b10d97bdd31927
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-02-26 20:08:17 +0000

    Add custom reward redemption listener and websocket event

---
+++ src/events/redemptions.ts
@@ -0,0 +1,46 @@
+import { UserByLoginResponse } from "./../data/types";
+import { tmi } from "../tmi";
+import { ChatUserstate } from "tmi.js";
+import { config } from "../config";
+import WebSocketServer from "../WebSocketServer";
+import { MoodChangePacket, MainframeEvent } from "p4nth3rb0t-types";
+
+const sendMoodChangeEvent = async (mood: string, rewardId: string) => {
+  try {
+    const moodChangeEvent: MoodChangePacket = {
+      event: MainframeEvent.moodChange,
+      id: rewardId,
+      data: {
+        mood,
+      },
+    };
+
+    WebSocketServer.sendData(moodChangeEvent);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+tmi.on(
+  "message",
+  async (
+    channel: string,
+    tags: ChatUserstate,
+    message: string,
+    self: boolean,
+  ) => {
+    const rewardId: string = tags["custom-reward-id"];
+
+    if (Object.keys(config.redemptions).includes(rewardId)) {
+      const mood = config.redemptions[rewardId];
+      const username = tags["username"];
+
+      await sendMoodChangeEvent(mood, rewardId);
+
+      tmi.say(
+        config.channel,
+        `whitep30PEWPEW Thanks for the **${mood}** mood change, @${username}! whitep30PEWPEW`,
+      );
+    }
+  },
+);
