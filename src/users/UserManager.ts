c69b3ce23c5c18d09e873a08c1001b168f347c5a
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-03-26 14:47:17 +0000

    Only send logoUrl with user obj if they pass an age check of > 7 days

--- src/users/UserManager.ts
+++ src/users/UserManager.ts
@@ -1,6 +1,21 @@
 import axios from "axios";
 import { UserByLoginResponse, UserByIdResponse } from "../data/types";
 
+// TODO = AGE CACHE THING!
+// currently we can restart the bot to clear the cache
+// but right now if someone does not pass age check
+// then they will never have a logo shown
+
+const accountIsOlderThanSevenDays = (createdAt: string): boolean => {
+  const SEVEN_DAYS = 604800;
+  const accountCreated = new Date(createdAt).valueOf();
+  const accountCreatedEpoch = accountCreated / 1000;
+  const now = new Date().valueOf();
+  const epoch = now / 1000;
+
+  return epoch - accountCreatedEpoch > SEVEN_DAYS;
+};
+
 export default class UserManager {
   static cache = new Map<string, any>();
 
@@ -16,8 +31,16 @@ export default class UserManager {
           accept: "application/vnd.twitchtv.v5+json",
           "client-id": process.env.CLIENT_ID,
         },
-      }
+      },
     );
+
+    const passesAgeCheck = accountIsOlderThanSevenDays(
+      response.data.created_at,
+    );
+
+    if (!passesAgeCheck) {
+      response.data.logo = "";
+    }
 
     this.cache.set(login, response.data);
 
@@ -36,11 +59,18 @@ export default class UserManager {
           accept: "application/vnd.twitchtv.v5+json",
           "client-id": process.env.CLIENT_ID,
         },
-      }
+      },
     );
 
-    this.cache.set(userId, response.data);
+    const passesAgeCheck = accountIsOlderThanSevenDays(
+      response.data.created_at,
+    );
 
+    if (!passesAgeCheck) {
+      response.data.logo = "";
+    }
+
+    this.cache.set(userId, response.data);
     return response.data;
   }
 }
