559f8e4fcec78186e414081c9327d3746d5fab8c
Salma Alam-Naylor <whitep4nth3r@gmail.com>
2021-07-12 13:13:25 +0100

    Add a more VERBOSE chat shout out for streamers as requested by MattyNewShoes

--- src/events/shoutout.ts
+++ src/events/shoutout.ts
@@ -26,12 +26,12 @@ export const sendShoutoutEvent = async (messageId: string, message: string) => {
         user.users[0]._id,
       );
 
-      tmi.say(
-        config.channel,
-        `whitep30PEWPEW Go check out ${user.users[0].display_name} at https://www.twitch.tv/${user.users[0].name} and give them some panther PEW PEWS! whitep30PEWPEW`,
-      );
-
       if (channel) {
+        tmi.say(
+          config.channel,
+          `whitep30PEWPEW Go check out ${user.users[0].display_name} at https://www.twitch.tv/${user.users[0].name} and give them some panther PEW PEWS! They were last seen streaming: ${channel.title} in ${channel.game_name} whitep30PEWPEW`,
+        );
+
         const dataToSend: ShoutOutData = {
           lastStream: {
             title: channel.title,
